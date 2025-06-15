3ad3f9b4ad5aa1879b9c071d411c056f7d117a7a
ZuanVFX <zuanvfx@gmail.com>
2025-06-15 13:07:24 +0700

    Delete shellbackup directory

--- shellbackup/[Auto] Ufshc Governor Maximum.sh
+++
@@ -1,3 +0,0 @@
-pm disable --user 0 com.miui.powerkeeper/.thermal.ThermalService
-
-pm disable --user 0 com.miui.powerkeeper/.statemachine.PowerStateMachineService
--- shellbackup/[Auto]backup.sh
+++
@@ -1,2 +0,0 @@
-dd if=/dev/block/by-name/boot_a of=/sdcard/boot.img
-dd if=/dev/block/by-name/vendor_boot_a of=/sdcard/vendor_boot.img
